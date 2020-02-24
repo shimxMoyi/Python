16e4d596846797fc6ce118790548cf91419719bb
ydzydzydz <a25112512@gmail.com>
2020-02-24 23:04:37 +0800

    update

--- python_11.py
+++ python_11.py
@@ -17,16 +17,15 @@ def get_weather():
     weather = result.json()['weatherinfo']
     return [weather['city'], weather['temp'], weather['WD'], weather['WS'], weather['SD'], weather['time']]
 
-def get_secret():
+def get_secret(secret):
     timestamp = int(round(time.time() * 1000))
-    secret = 'SECa3cdb0fcd73cbdddba6ebc8f924342aab24b5efef3251b4b239d3470e21f6b4c'
     string_to_sign = '%s\n%s' % (timestamp, secret)
     hmac_code = hmac_code = hmac.new(secret.encode('utf-8'), string_to_sign.encode('utf-8'), digestmod=hashlib.sha256).digest()
     sign = quote_plus(base64.b64encode(hmac_code))
     return [timestamp, sign]
 
-def ding_talk(weather, secret):
-    url = 'https://oapi.dingtalk.com/robot/send?access_token=51637853748be7fb18413312250d28cc82fe7904966b0a649da28d5a94eaeae4&timestamp=%s&sign=%s' % (secret[0], secret[1])
+def ding_talk(url, secret, weather):
+    url = url % (secret[0], secret[1])
     headers = {'Content-Type': 'application/json; charset=utf-8'}
     data = {
         "msgtype": "markdown",
@@ -34,13 +33,15 @@ def ding_talk(weather, secret):
             "title": "今日天气情况",
             "text": "**城市:** %s \t\t" % weather[0] + "**温度:** %s ℃ \n\n" % weather[1] +
                     "**风向:** %s \t\t" % weather[2] + "**风力:** %s \n\n" % weather[3] +
-                    "**湿度:** %s \t\t" % weather[4] + "**时间:** %s \n\n" % weather[5]
+                    "**湿度:** %s \t\t" % weather[4] + "**更新:** %s \n\n" % weather[5]
         }
     }
     r = requests.post(url, data=json.dumps(data), headers=headers)
 
 if __name__ == '__main__':
+    secret = 'SECa3cdb0fcd73cbdddba6ebc8f924342aab24b5efef3251b4b239d3470e21f6b4c'
+    url = 'https://oapi.dingtalk.com/robot/send?access_token=51637853748be7fb18413312250d28cc82fe7904966b0a649da28d5a94eaeae4&timestamp=%s&sign=%s'
     weather = get_weather()
-    secret = get_secret()
-    ding_talk(weather, secret)
+    secret = get_secret(secret)
+    ding_talk(url, secret, weather)
 
