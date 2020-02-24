9b344cd3b7f9f5827973b5602e7e9d498020724e
ydzydzydz <a25112512@gmail.com>
2020-02-24 13:25:44 +0800

    update

---
+++ python_06.py
@@ -0,0 +1,10 @@
+import getpass
+
+username = input('username: ')
+password = getpass.getpass('password: ')
+
+if username == 'Tom' and password == '123456'
+    print('Login successful')
+else: 
+    print('Login incorrect')
+
