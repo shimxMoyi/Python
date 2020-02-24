cb7efec0c8f42f954d01cffa2d551c22301b6a6c
ydzydzydz <a25112512@gmail.com>
2020-02-24 18:19:34 +0800

    update

---
+++ python_09.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+'''ansible [hosts] -m python_09 -a "yuan=[src] mubiao=[dst]"'''
+
+from ansible.module_utils.basic import AnsibleModule
+import shutil
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            yuan=dict(required=True, type='str'),
+            mubiao=dict(required=True, type='str')
+        )
+    )
+    shutil.copy(module.params['yuan'], module.params['mubiao'])
+    module.exit_json(changed=True)
+
+if __name__ == '__main__':
+    main()
